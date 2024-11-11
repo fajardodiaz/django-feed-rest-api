e0bff99d809732e7d32910e129b3483d5add23d5
Pablo Fajardo <pfajardodiaz17@gmail.com>
2024-11-11 12:18:36 -0500

    Add token auth and permissions to allow user to edit his own profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,14 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers, models
+from profiles_api import serializers, models, permissions
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating user profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API Viewset"""
